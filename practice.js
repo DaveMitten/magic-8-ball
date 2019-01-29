a1b3f78b50c87650a3d2e766668d78e336abfc90
DaveMitten <davidmitten88@gmail.com>
2019-01-29 08:18:33 +0000

    input now collected. Need to output data to dom and style

--- practice.js
+++ practice.js
@@ -2,82 +2,30 @@
 //collection of answers
 
 
-//random answer pushed to an empty array and then returned.
 function askingAQuestion() {
-	// let questionAsked = questionForm.questionInput.value;
-	// alert(questionAsked);
-	let answers = ["Nope", "For sure!", "Not in  a million years", "YES", "Really? Do you need to ask"]
-
-
-	let randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-
-	let result = [];
-	result.push(randomAnswer);
-	console.log(result);
-
-	return result;
+  const questionAsked = document.getElementById('question').value;
  
+console.log(questionAsked);
 
+  const answers = [
+    'Nope',
+    'For sure!',
+    'Not in  a million years',
+    'YES',
+    'Really? Do you need to ask'
+  ];
 
+  const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
 
+  const result = [];
+  result.push(randomAnswer);
+  console.log(result);
+
+  return result;
 }
 
 function init() {
-	const resultToScreen = document.getElementById("screenId");
+  const resultToScreen = document.querySelector('#screenId');
 
-	resultToScreen.innerText = askingAQuestion ;
-
+  resultToScreen.innerText = askingAQuestion;
 }
-
-
-
-
-
-
-
-
-// //reset - needs to empty array
-// window.addEventListener(click, function emptyMe(){
-// 		//array.remove;
-// 		// return array;
-
-// });
-
-// function remove(event){
-
-// }
-
-
-
-
-
-
-
-
-
-// window.addEventListener(click, random());
-
-
-
-
-
-
-//**************************************practice of high order functions****************************
-
-// use the reduce method in combination with the concat method to “flatten” an
-//  array of arrays into a single array that has all the elements of the original arrays.
-// // 
-
-
-// let arrays = [[1, 2, 3], [4, 5], [6]];
-
-
-// function flatten(){
-
-// }
-
-
-
-
-
-
